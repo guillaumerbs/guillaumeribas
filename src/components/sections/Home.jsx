912c56bac545e78b332e6533503705c365d8d0f0
Guillaume Ribas <guillaume.ribas@hotmail.fr>
2025-03-12 16:10:50 +0100

    add buttons to routes & socials

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,3 +1,5 @@
+import { FaGithub, FaLinkedin, FaFileDownload } from "react-icons/fa";
+
 export const Home = () => {
     return (
         <section 
@@ -11,7 +13,26 @@ export const Home = () => {
                 <p className="text-gray text-lg mb-8 max-w-lg mx-auto">
                     Développeur Full Stack, je conçois des applications performantes et intuitives. Toujours en quête d'optimisation et d'innovation, j'accorde une attention particulière à l'expérience utilisateur et aux bonnes pratiques de développement.
                 </p>
+                <div className="flex justify-center space-x-4 mb-8">
+                    <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]">
+                        Mes Projets
+                    </a>
+                    <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10">
+                        Contactez-Moi
+                    </a>
+                </div>
+                <div className="flex justify-center space-x-10">
+                    <a href="https://github.com/guillaumerbs" target="_blank" rel="noopener noreferrer" className="text-gray-300 text-4xl hover:text-green-700">
+                        <FaGithub />
+                    </a>
+                    <a href="https://www.linkedin.com/in/guillaumeribas" target="_blank" rel="noopener noreferrer" className="text-gray-300 text-4xl hover:text-blue-500">
+                        <FaLinkedin />
+                    </a>
+                    <a href="/cv.pdf" download className="text-gray-300 text-4xl hover:text-red-600">
+                        <FaFileDownload />
+                    </a>
+                </div>
             </div>
-    </section>
+        </section>
     );
 };
