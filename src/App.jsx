33b5d472783938f537b19b0ea4f0a8b46bc680f7
Guillaume Ribas <guillaume.ribas@hotmail.fr>
2025-03-12 13:26:30 +0100

    start navbar design and import in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import './App.css'
-import { LoadingScreen } from './components/LoadingScreen';
 import './index.css';
+import { LoadingScreen } from './components/LoadingScreen';
+import { Navbar } from './components/Navbar';
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -15,7 +16,7 @@ function App() {
             isLoaded ? "opacity-100" : "opacity-0"
           } bg-black text-gray-100`}
         >
-
+          <Navbar />
         </div>
     </>
   );
