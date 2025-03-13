857824d881a800f75ea0dc75ecb1d7fa53581f63
Guillaume Ribas <guillaume.ribas@hotmail.fr>
2025-03-13 12:31:39 +0100

    add skills section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,10 +1,89 @@
 export const About = () => {
+    const softSkills = [
+        "Communication",
+        "Esprit d'équipe",
+        "Autonomie",
+        "Gestion du temps",
+        "Créativité",
+        "Résolution de problèmes"
+    ];
+
+    const frontendSkills = [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React",
+        "Tailwind CSS",
+        "Bootstrap",
+        "Twig",
+    ];
+
+    const backendSkills = [
+        "PHP",
+        "Symfony",
+        "Laravel",
+        "Node.js",
+        "MySQL",
+    ];
+
     return (
         <section 
         id="about" 
         className="min-h-screen flex items-center justify-center py-20">
+            <div className="max-w-3xl mx-auto px-4">
+                <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center"> 
+                &#192; propos
+                </h2>
 
+                <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
 
+                        <div className="flex flex-col gap-6">
+                            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                                <h3 className="text-2xl font-bold mb-4">Soft Skills</h3>
+                                <div className="flex flex-wrap gap-2">
+                                    {softSkills.map((skill, key) => (
+                                        <span key={key} className="bg-blue-500/10 text-white px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 2246, 0.2)] transition">
+                                            {skill}
+                                        </span>
+                                    ))}
+                                </div>
+                            </div>
+                            
+                            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                                <h3 className="text-xl font-bold mb-4">Frontend</h3>
+                                <div className="flex flex-wrap gap-2">
+                                    {frontendSkills.map((tech, key) => (
+                                        <span key={key} className="bg-blue-500/10 text-white px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 2246, 0.2)] transition">
+                                            {tech}
+                                        </span>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col gap-6">
+                            <div className="flex justify-center p-6 hover:-translate-y-1 transition-all">
+                                <div className="w-38 h-46 rounded-md overflow-hidden border-3 border-blue-500/20">
+                                    <img src="/profil.png" alt="Guillaume Ribas" className="w-full h-full object-cover" />
+                                </div>
+                            </div>
+                            
+                            {/* Backend Skills */}
+                            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                                <h3 className="text-xl font-bold mb-4">Backend</h3>
+                                <div className="flex flex-wrap gap-2">
+                                    {backendSkills.map((tech, key) => (
+                                        <span key={key} className="bg-blue-500/10 text-white px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 2246, 0.2)] transition">
+                                            {tech}
+                                        </span>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
