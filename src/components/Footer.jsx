5214a6705e40827099287baa6434c3b98fceeba3
Guillaume Ribas <guillaume.ribas@hotmail.fr>
2025-03-14 17:19:01 +0100

    setup and display footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <footer className="fixed bottom-0 w-full text-center bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg py-3">
+            <p className="text-gray-300 font-mono text-sm">
+                Guillaume <span className="text-blue-500">Ribas</span> 2025 — Made with love
+            </p>
+        </footer>
+    );
+};
