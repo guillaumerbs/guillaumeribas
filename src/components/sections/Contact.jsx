3ca06d33f8158044bab30b4569e94cd33c7908eb
Guillaume Ribas <guillaume.ribas@hotmail.fr>
2025-03-14 15:00:11 +0100

    setup contact component & import

---
+++ src/components/sections/Contact.jsx
@@ -0,0 +1,11 @@
+export const Contact = () => {
+    return (
+        <section id="contact" className="min-h-screen flex flex-items-center justify-center py-20"> 
+        <div className="px-4 w-150">
+            <h2 className="text-3xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent text-center">Me Contacter</h2>
+
+        </div>
+
+        </section>
+    );
+};
