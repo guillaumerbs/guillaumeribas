6dca6e85f578cec9f351ae66ab639166c8a240f2
Guillaume Ribas <guillaume.ribas@hotmail.fr>
2025-03-14 18:28:48 +0100

    fix contact form display on smaller devices

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -65,7 +65,7 @@ export const Contact = () => {
     return (
         <section id="contact" className="min-h-screen flex flex-items-center justify-center py-20">
             <RevealOnScroll>
-            <div className="px-4 w-150">
+            <div className="px-4 w-70 md:w-150">
                 <h2 className="text-3xl font-bold mb-6 bg-gradient-to-r from-blue-700 to-blue-300 bg-clip-text text-transparent text-center">Me Contacter</h2>
                 <form className="space-y-6" onSubmit={handleSubmit}>
                     <div className="relative">
